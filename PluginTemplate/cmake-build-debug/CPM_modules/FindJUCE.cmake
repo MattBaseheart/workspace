d8be12dc05c7863f5171446451d437dbbc46c70c
Matt Baseheart <mbase123@gmail.com>
2024-12-11 20:14:58 -0500

    Added WolfSound Template

---
+++ PluginTemplate/cmake-build-debug/CPM_modules/FindJUCE.cmake
@@ -0,0 +1,3 @@
+include("C:/Users/mbase/workspace/PluginDev/workspace/PluginTemplate/libs/cpm/CPM_0.40.2.cmake")
+CPMAddPackage("NAME;JUCE;GIT_TAG;8.0.2;VERSION;8.0.2;GITHUB_REPOSITORY;juce-framework/JUCE;SOURCE_DIR;C:/Users/mbase/workspace/PluginDev/workspace/PluginTemplate/libs/juce")
+set(JUCE_FOUND TRUE)
